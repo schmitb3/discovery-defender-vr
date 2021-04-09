3953107028340c7a417b61b252b26408e79cd187
schmitb3 <48733409+schmitb3@users.noreply.github.com>
2021-04-09 16:58:36 -0400

    Adding initial files

---
+++ Discovery Defender VR/Assets/GunScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using OVR;
+using Oculus;
+
+public class GunScript : MonoBehaviour
+{
+    float timeUntilFire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
